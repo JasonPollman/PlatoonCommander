917298fb922d998868e040f247f157b20d7bc822
Jason Pollman <JPPuffNStuff@gmail.com>
2014-09-23 17:02:06 -0400

    Title Menu

---
+++ Assets/Cursor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetCursor : MonoBehaviour {
+
+	Texture2D 	cursorTexture 	= (Texture2D)Resources.Load("Cursor.png");
+	CursorMode 	cursorMode 		= CursorMode.Auto;
+	Vector2 	hotSpot 		= Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/TitleStartAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleStartAction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// On Mouse Down
+	void OnMouseDown() {
+		Application.LoadLevel("Map-1");
+	}
+}
